e57c4d386720b27e09fbfe54bbdb664c741177bb
anargasimzada <tu7g11auv@code.edu.az>
2024-04-02 01:39:47 +0400

    enccar

---
+++ EncapsulationCar/Models/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationCar.Models
+{
+    internal class Person
+    {
+        private sbyte _age;
+        //property
+        public sbyte Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Age must be equal or greater than 0");
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+        
+        public string Name { get; set; }
+        public Person(sbyte age)
+        {
+            Age = age;
+        }
+    }
+}
