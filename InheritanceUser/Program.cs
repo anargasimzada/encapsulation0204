5ed537d7098481e9b7ff87f190bd44aa9049b0eb
anargasimzada <tu7g11auv@code.edu.az>
2024-04-02 01:41:56 +0400

    encapuser

---
+++ InheritanceUser/Program.cs
@@ -0,0 +1,15 @@
+﻿using InheritanceUser.Models;
+
+namespace InheritanceUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("saBir   ", "   guLiEv", "Sabir123");
+            user.ChangePassword("Sabir123", "uliyev123");
+
+            //Console.WriteLine(user.UserName);
+        }
+    }
+}
